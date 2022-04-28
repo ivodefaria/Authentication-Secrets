4a5a8afe4d723a91e975ee77a92558d12f2bd853
ivofaria <ivojunglist@gmail.com>
2022-04-28 10:38:06 +0100

    Level 2 - Encryption

--- User.js
+++ User.js
@@ -1,10 +1,15 @@
 // jshint esversion: 8
 
 const mongoose = require('mongoose');
+const encrypt = require("mongoose-encryption");
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
+
+const secret = "Thisisourlittlesecret.";
+
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
 
 module.exports = mongoose.model("User", userSchema);
