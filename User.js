5ee678c6eb43ca70d05916cf90e6a68822461f09
ivofaria <ivojunglist@gmail.com>
2022-04-28 10:43:34 +0100

    Add Environment Vars And Gitignore

--- User.js
+++ User.js
@@ -1,5 +1,6 @@
 // jshint esversion: 8
 
+require("dotenv").config();
 const mongoose = require('mongoose');
 const encrypt = require("mongoose-encryption");
 
@@ -8,8 +9,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = "Thisisourlittlesecret.";
-
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 module.exports = mongoose.model("User", userSchema);
